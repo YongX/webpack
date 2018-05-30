112b50231ee4585ac41caf92c14f0c020ef25809
Xiang Yong <ivan.xyong@gmail.com>
2018-05-30 17:47:15 +0800

    modify html template

--- template/build/webpack.dev.conf.js
+++ template/build/webpack.dev.conf.js
@@ -55,6 +55,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'index.html',
+      dev: true,
       inject: true
     }),
     // copy custom static assets
